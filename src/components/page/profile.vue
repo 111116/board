4bb4b2c46fc5bd6fd09bec2fee8370a084e2148e
bilibili <fenggaobuke@gmail.com>
2021-03-17 16:45:53 +0800

    add avatar cropper

--- src/components/page/profile.vue
+++ src/components/page/profile.vue
@@ -1,22 +1,35 @@
 <!--login page-->
 <template>
-    <div>123</div>
+    <div class="text-center">
+        <img v-if="userAvatar" :src="userAvatar">
+        <el-button id="pick-avatar">上传图片</el-button>
+        <avatar-cropper
+            @uploaded="handleUploaded"
+            trigger="#pick-avatar"
+            upload-url="/api/img/upload" />
+    </div>
 </template>
 <script>
 // import httpRequest from "@/utils/communication"
+import AvatarCropper from "vue-avatar-cropper"
 export default {
     name: 'profile',
     components: {
+        AvatarCropper
     },
     data(){
         return{
             email: "",
             password: "",
             success: false,
+            userAvatar: undefined,
         }
     },
     methods: {
-
+        handleUploaded(resp) {
+            // this.userAvatar = resp.relative_url;
+            console.log(resp)
+        }
     },
 }
 </script>
