2979ad02b921fa7470aefb34408d6c496d78e631
bilibili <fenggaobuke@gmail.com>
2021-04-09 16:10:49 +0800

    dev api proxy use backend@sv

--- vue.config.js
+++ vue.config.js
@@ -3,7 +3,7 @@ module.exports = {
     disableHostCheck: true,
     proxy: {
       '/api': {
-        target: 'http://115.182.62.165:5000', // 修改为你的Django服务器地址
+        target: 'http://45.63.85.11:5000', // 修改为你的Django服务器地址
         changeOrigin: true,
         pathRewrite:{
           '^/api':''
